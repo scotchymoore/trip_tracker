5faf31bf6bd5c4453799ae5dc5ee67987a3bf137
Alan Vincent <alan.d.vincent@gmail.com>
2017-06-04 14:07:24 -0600

    built locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,50 @@
 class LocationsController < ApplicationController
+before_action: set_location, except[:index, :new, :create, :update)
+
   def index
+    @locations = trip.locations
   end
 
   def show
   end
 
   def new
+    @location = trip.locations.new
+  end
+
+  def create
+    @location = trip.locatons.new(location_params)
+      if @location.save
+        redirect_to locations_path
+      else 
+        render :new
+      end
   end
 
   def edit
   end
+
+  def update
+    if trip.location.save
+      redirect_to locations_path
+    else
+      render :edit
+    end
+  end
+
+  def destory
+    @location.destroy
+    redirect_to locations_path
+  end
+
+private
+
+def location_params
+  params.require(:location).permit(:name, :date)
+end
+
+def set_location
+  @location = trips.location.find(params[:id])
+end
+
 end
